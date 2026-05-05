0779f9f45851f53246f26d7522fff5c24d9c87be
purpleshellsecurity <tenant@purpleshellsecurity.com>
2026-05-04 22:40:20 -0400

    fixing

--- detections/T1098.006-cluster-role-binding/creategroup.ps1
+++ detections/T1098.006-cluster-role-binding/creategroup.ps1
@@ -2,8 +2,14 @@
 az ad group create--display-name "AKS-AdversaryLab-Admins" --mail-nickname "aks-adversarylab-admins"
 
 # Add yourself to it
-MY_OBJECT_ID=$(az ad signed-in-user show --query id -o tsv)
+
+$MY_OBJECT_ID=$(az ad signed-in-user show --query id -o tsv)
 az ad group member add --group "AKS-AdversaryLab-Admins" --member-id "$MY_OBJECT_ID"
 
 # Get the Object ID to paste into the workflow
 az ad group show --group "AKS-AdversaryLab-Admins" --query id -o tsv
+
+
+az monitor log-analytics workspace show --resource-group <rg-name>
+  --workspace-name <workspace-name> \
+  --query id -o tsv
