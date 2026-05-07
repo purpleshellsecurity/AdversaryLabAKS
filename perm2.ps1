0a402f961ca0358d2faab274e69d2c33875b5621
purpleshellsecurity <tenant@purpleshellsecurity.com>
2026-05-07 19:33:16 -0400

    chore: remove leftover testing scripts

--- perm2.ps1
+++
@@ -1,5 +0,0 @@
-$WORKSPACE_RG="adversary-lab-rg2"
-$SUBSCRIPTION_ID="194065fa-04a5-4390-bf20-f648148de4af"
-$SP_OBJECT_ID="ba0d4238-270e-4895-b51d-045993ce6049"
-
-az role assignment list --assignee "$SP_OBJECT_ID" --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$WORKSPACE_RG" -o table
